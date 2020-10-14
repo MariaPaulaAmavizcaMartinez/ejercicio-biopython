56938f624cb563ee3bb345ef1c0c731832f51b8c
MariaPaulaAmavizcaMartinez <72842364+MariaPaulaAmavizcaMartinez@users.noreply.github.com>
2020-10-14 00:17:51 -0600

    Delete Script.py

--- Script.py
+++
@@ -1,8 +0,0 @@
-from Bio.Seq import Seq
-from Bio.SeqFeatures import SeqFeature, FeatureLocation
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-def summarize_contents(filename):
-        record = SeqIO.read ("filename", "genbank")
-        print(record)
-        
